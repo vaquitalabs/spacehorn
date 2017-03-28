d89f06bf4c7a76ea5a2e53a5b92dd9af0c8c3dfa
Marcos Navarro <marcos@aftertype.com>
2017-03-27 23:20:02 -0700

    Improved middleware adaptation

--- dist/applyMiddleware.js
+++ dist/applyMiddleware.js
@@ -9,9 +9,23 @@ var _error = require('./messages/error');
 function applyMiddleware(app, drawer, middleware) {
 	if (middleware.constructor === Array) {
 		var _loop = function _loop(i) {
-			app.use(function (req, res, next) {
-				middleware[i](drawer, req, res, next);
-			});
+			var mddw = middleware[i];
+			var mddwFunc = mddw.constructor === Array ? mddw.run : mddw;
+			var mddwArgs = mddwFunc.toString().split('{')[0].match(/\(([^)]*)\)/)[1].split(',');
+			var mddwReady = mddwFunc;
+			var mddwHasDrawer = mddwArgs.length > 3;
+
+			if (mddwHasDrawer) {
+				mddwReady = function mddwReady(req, res, next) {
+					mddwFunc(drawer, req, res, next);
+				};
+			}
+
+			if (mddw.constructor === Array) {
+				app.use(mddw.path, mddwReady);
+			} else {
+				app.use(mddwReady);
+			}
 		};
 
 		for (var i = 0; i < middleware.length; i++) {
